45b625c2ead5c31732be7e17a7a0a8cf9c19d0e7
Fatemeh <fa.damirchy@gmail.com>
2024-12-28 14:53:28 +0330

    added some visual styles to the main menu

--- src/main_menu.c
+++ src/main_menu.c
@@ -12,20 +12,44 @@ void show_main_menu()
         "Sign up",
         "Exit"};
 
+    // color settings
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+    init_pair(4, COLOR_YELLOW, COLOR_BLACK);
+
+    // getting console size
+    int max_y, max_x;
+    getmaxyx(stdscr, max_y, max_x);
+
     while (1)
     {
         clear();
-        printw("=== Main Menu ===\n");
+
+        // find center to start showing the menu
+        int start_y = (max_y / 2) - (NUM_CHOICES / 2);
+        int start_x = (max_x / 2) - 10;
+
+        // displaying the menu
+        // ********** START **********
+        attron(COLOR_PAIR(3));
+        mvprintw(start_y - 2, start_x, "=== Main Menu ===");
+        attroff(COLOR_PAIR(3));
 
         // making the choice highlighted
         For(i, NUM_CHOICES)
         {
             if (i == choice)
             {
-                attron(A_REVERSE);
+                attron(COLOR_PAIR(2));
             }
-            printw("%s\n", choices[i]);
-            attroff(A_REVERSE);
+            else
+            {
+                attron(COLOR_PAIR(1));
+            }
+            mvprintw(start_y + i, start_x, "%s", choices[i]);
+            attroff(COLOR_PAIR(1) | COLOR_PAIR(2));
         }
 
         // moving in the list
@@ -47,20 +71,27 @@ void show_main_menu()
         case ENTER:
             if (choice == 0)
             {
+                attron(COLOR_PAIR(4));
                 /*Login option*/
             }
             else if (choice == 1)
             {
+                attron(COLOR_PAIR(4));
                 /*Sign up option*/
             }
             else if (choice == 2)
             {
-                /*Exit option*/
+                attron(COLOR_PAIR(4));
+                mvprintw(start_y + NUM_CHOICES + 1, start_x, "Exiting...");
+                attroff(COLOR_PAIR(3));
+                refresh();
+                return;
             }
             break;
 
         default:
             break;
         }
+        // ********** END **********
     }
 }
