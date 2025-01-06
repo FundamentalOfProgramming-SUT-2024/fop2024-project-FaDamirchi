de6e2bba938e7bb8d156024554f0a7722c9beb7f
Fatemeh <fa.damirchy@gmail.com>
2025-01-06 15:27:12 +0330

    now the pregame menu is different for new users and old users

--- include/pregame_menu.h
+++ include/pregame_menu.h
@@ -2,7 +2,8 @@
 #define PREGAME_MENU
 
 // consts
-#define NUM_CHOICES_PREGAME 4
+#define NUM_CHOICES_DEFAULT 4
+#define NUM_CHOICES_NEW_USER 3
 
 // functions
 void show_pregame_menu(char *username);
--- src/pregame_menu.c
+++ src/pregame_menu.c
@@ -6,80 +6,158 @@
 
 void show_pregame_menu(char *username)
 {
-    int choice = 0;
-
-    const char *options[NUM_CHOICES_PREGAME] = {
-        "New game",
-        "Resume game",
-        "Score board",
-        "Settings"};
-
-    init_colors();
-
-    // getting console size
-    int max_y, max_x;
-    getmaxyx(stdscr, max_y, max_x);
-
-    curs_set(0); // hiding cursor
-
-    // displaying the main menu
-    // ********* START **********
-    while (1)
+    if (username == NULL)
     {
-        clear();
+        const char *options[NUM_CHOICES_NEW_USER] = {
+            "Start game",
+            "Score board",
+            "Settings"};
 
-        // find center to start showing the menu
-        int start_y = (max_y / 2) - (NUM_CHOICES_PREGAME / 2);
-        int start_x = (max_x / 2) - 10;
+        int choice = 0;
 
-        // displaying the menu
-        int height = NUM_CHOICES_PREGAME + 1;
-        int width = strlen("=== Pregame Menu ===");
-        draw_border(start_y, start_x, height, width);
+        init_colors();
 
-        show_title(start_y - 2, start_x, "=== Pregame Menu ===");
+        // getting console size
+        int max_y, max_x;
+        getmaxyx(stdscr, max_y, max_x);
 
-        highlight_choice(start_y, start_x, options, NUM_CHOICES_PREGAME, choice);
+        curs_set(0); // hiding cursor
 
-        // moving in the list
-        int ch = getch();
-        switch (ch)
+        // displaying the main menu
+        // ********* START **********
+        while (1)
         {
-        case KEY_UP:
-            choice--;
-            if (choice < 0)
-                choice = NUM_CHOICES_PREGAME - 1; // loop to the last option
-            break;
+            clear();
 
-        case KEY_DOWN:
-            choice++;
-            if (choice >= NUM_CHOICES_PREGAME)
-                choice = 0; // loop to the first option
-            break;
+            // find center to start showing the menu
+            int start_y = (max_y / 2) - (NUM_CHOICES_NEW_USER / 2);
+            int start_x = (max_x / 2) - 10;
 
-        case ENTER:
-            if (choice == 0)
-            {
-                // start new game
-            }
-            else if (choice == 1)
-            {
-                // resume old game
-            }
-            else if (choice == 2)
-            {
-                // show score board
-            }
+            // displaying the menu
+            int height = NUM_CHOICES_NEW_USER + 1;
+            int width = strlen("=== Pregame Menu ===");
+            draw_border(start_y, start_x, height, width);
 
-            else if (choice == 3)
-            {
-                // show settings
-            }
-            break;
+            show_title(start_y - 2, start_x, "=== Pregame Menu ===");
 
-        default:
-            break;
+            highlight_choice(start_y, start_x, options, NUM_CHOICES_NEW_USER, choice);
+
+            // moving in the list
+            int ch = getch();
+            switch (ch)
+            {
+            case KEY_UP:
+                choice--;
+                if (choice < 0)
+                    choice = NUM_CHOICES_NEW_USER - 1; // loop to the last option
+                break;
+
+            case KEY_DOWN:
+                choice++;
+                if (choice >= NUM_CHOICES_NEW_USER)
+                    choice = 0; // loop to the first option
+                break;
+
+            case ENTER:
+                if (choice == 0)
+                {
+                    // start new game
+                }
+                else if (choice == 1)
+                {
+                    // resume old game
+                }
+                else if (choice == 2)
+                {
+                    // show score board
+                }
+
+                break;
+
+            default:
+                break;
+            }
         }
+        // ********* END **********
     }
-    // ********* END **********
+
+    else
+    {
+        int choice = 0;
+
+        const char *options[NUM_CHOICES_DEFAULT] = {
+            "New game",
+            "Resume game",
+            "Score board",
+            "Settings"};
+
+        init_colors();
+
+        // getting console size
+        int max_y, max_x;
+        getmaxyx(stdscr, max_y, max_x);
+
+        curs_set(0); // hiding cursor
+
+        // displaying the main menu
+        // ********* START **********
+        while (1)
+        {
+            clear();
+
+            // find center to start showing the menu
+            int start_y = (max_y / 2) - (NUM_CHOICES_DEFAULT / 2);
+            int start_x = (max_x / 2) - 10;
+
+            // displaying the menu
+            int height = NUM_CHOICES_DEFAULT + 1;
+            int width = strlen("=== Pregame Menu ===");
+            draw_border(start_y, start_x, height, width);
+
+            show_title(start_y - 2, start_x, "=== Pregame Menu ===");
+
+            highlight_choice(start_y, start_x, options, NUM_CHOICES_DEFAULT, choice);
+
+            // moving in the list
+            int ch = getch();
+            switch (ch)
+            {
+            case KEY_UP:
+                choice--;
+                if (choice < 0)
+                    choice = NUM_CHOICES_DEFAULT - 1; // loop to the last option
+                break;
+
+            case KEY_DOWN:
+                choice++;
+                if (choice >= NUM_CHOICES_DEFAULT)
+                    choice = 0; // loop to the first option
+                break;
+
+            case ENTER:
+                if (choice == 0)
+                {
+                    // start new game
+                }
+                else if (choice == 1)
+                {
+                    // resume old game
+                }
+                else if (choice == 2)
+                {
+                    // show score board
+                }
+
+                else if (choice == 3)
+                {
+                    // show settings
+                }
+                break;
+
+            default:
+                break;
+            }
+        }
+        // ********* END **********
+    }
 }
